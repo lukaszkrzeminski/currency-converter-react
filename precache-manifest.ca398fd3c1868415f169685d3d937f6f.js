907adb2d06a72dbb5dc4036e450a1a9bf6e0ad7f
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-08-02 14:21:38 +0200

    Updates

---
+++ precache-manifest.ca398fd3c1868415f169685d3d937f6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b33442d1d1f6d7b6e2ca59f0615295",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "42839b05cd4dc97a261c",
+    "url": "/currency-converter-react/static/css/main.07ba6e24.chunk.css"
+  },
+  {
+    "revision": "cb70966e9e6cb160b785",
+    "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.59662f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42839b05cd4dc97a261c",
+    "url": "/currency-converter-react/static/js/main.bb0fb9e2.chunk.js"
+  },
+  {
+    "revision": "07b2973be230e9c120d7",
+    "url": "/currency-converter-react/static/js/runtime-main.2415008a.js"
+  },
+  {
+    "revision": "901ede01a502e7edff4310d426159530",
+    "url": "/currency-converter-react/static/media/background.901ede01.jpg"
+  }
+]);
