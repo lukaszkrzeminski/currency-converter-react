98032c363de58db39aea31536ee50b77ae4f888f
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-08-02 13:54:24 +0200

    added interactions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Background from "./Background";
 import Header from "./Header";
 import Main from "./Main";
@@ -6,11 +6,57 @@ import Form from "./Form";
 import Footer from "./Footer";
 
 function App() {
+  const [newUserValue, setNewUserValue] = useState("");
+  const [startCurrency, setStartCurrency] = useState("PLN");
+  const [finalCurrency, setFinalCurrency] = useState("PLN");
+  const [finalValue, setFinalValue] = useState("");
+
+  const computeResult = (startCurrency, finalCurrency, newUserValue, finalValue) => {
+    const PLNtoEUR = 0.22;
+    const PLNtoUSD = 0.25;
+    const EURtoUSD = 1.12;
+    const EURtoPLN = 1 / PLNtoEUR;
+    const USDtoPLN = 1 / PLNtoUSD;
+    const USDtoEUR = 1 / EURtoUSD;
+
+    switch (startCurrency + finalCurrency) {
+      case "PLNEUR":
+        setFinalValue(finalValue => (PLNtoEUR * newUserValue).toFixed(2))
+        break;
+      case "PLNUSD":
+        setFinalValue(finalValue => (PLNtoEUR * newUserValue).toFixed(2))
+        break;
+      case "EURUSD":
+        setFinalValue(finalValue => (EURtoUSD * newUserValue).toFixed(2))
+        break;
+      case "EURPLN":
+        setFinalValue(finalValue => (EURtoPLN * newUserValue).toFixed(2))
+        break;
+      case "USDEUR":
+        setFinalValue(finalValue => (USDtoEUR * newUserValue).toFixed(2))
+        break;
+      case "USDPLN":
+        setFinalValue(finalValue => (USDtoPLN * newUserValue).toFixed(2))
+        break;
+      default:
+        setFinalValue(finalValue => newUserValue)
+    }
+  }
+
   return (
     <Background>
       <Header title="Przelicz zanim wyjedziesz na zagraniczne wakacje!" />
       <Main>
-        <Form />
+        <Form
+        newUserValue={newUserValue}
+        setNewUserValue={setNewUserValue}
+        startCurrency={startCurrency}
+        setStartCurrency={setStartCurrency}
+        finalCurrency={finalCurrency}
+        setFinalCurrency={setFinalCurrency}
+        finalValue={finalValue}
+        setFinalValue={setFinalValue}
+        computeResult={computeResult}/>
       </Main>
       <Footer />
     </Background>
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,48 +1,75 @@
 import React from 'react';
 import "./style.css";
 
-const Form = () => (
-  <form>
-    <fieldset className="form__fieldset">
-      <legend className="form__legend">
-        Przelicznik Walut
-      </legend>
-      <p>
-        <label>
-          <span className="form__labelText">Ile chcę wymienić: </span>
-          <input
-            className="form__field form__field--value" type="number" step="0.01" min="0" required
-          />
-        </label>
-        <label>
-          <select className="form__field form__field--select" name="userCurrency">
-            <option value="PLN">PLN</option>
-            <option value="EUR">EUR</option>
-            <option value="USD">USD</option>
-          </select>
-        </label>
-      </p>
+const Form = ({ computeResult, newUserValue, setNewUserValue, startCurrency, setStartCurrency, finalCurrency, setFinalCurrency, finalValue }) => {
 
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    computeResult(startCurrency, finalCurrency, newUserValue, finalValue);
+  }
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">
+          Przelicznik Walut
+      </legend>
+        <p>
+          <label>
+            <span className="form__labelText">Ile chcę wymienić: </span>
+            <input
+              value={newUserValue}
+              onChange={(event) => setNewUserValue(event.target.value)}
+              className="form__field form__field--value" 
+              type="number" 
+              step="0.01" 
+              min="0" 
+              required
+            />
+          </label>
+          <label>
+            <select 
+            className="form__field form__field--select" 
+            name="userCurrency"
+            value={startCurrency}
+            onChange={(event) => setStartCurrency(event.target.value)}
+            >
+              <option value="PLN">PLN</option>
+              <option value="EUR">EUR</option>
+              <option value="USD">USD</option>
+            </select>
+          </label>
+        </p>
+
+        <p>
+          <label>
+            <span className="form__labelText">Ile dostanę: </span>
+            <input
+              className="form__field form__field--value form__field--final" 
+              type="number"
+              value={finalValue}
+              readOnly
+            />
+          </label>
+          <label>
+            <select 
+            className="form__field form__field--select" 
+            name="chosenCurrency"
+            value={finalCurrency}
+            onChange={(event) => setFinalCurrency(event.target.value)} 
+            required>
+              <option value="PLN">PLN</option>
+              <option value="EUR">EUR</option>
+              <option value="USD">USD</option>
+            </select>
+          </label>
+        </p>
+      </fieldset>
       <p>
-        <label>
-          <span className="form__labelText">Ile dostanę: </span>
-          <input
-            className="form__field form__field--value form__field--final" type="number" readOnly
-          />
-        </label>
-        <label>
-          <select className="form__field form__field--select" name="chosenCurrency" required>
-            <option value="PLN">PLN</option>
-            <option value="EUR">EUR</option>
-            <option value="USD">USD</option>
-          </select>
-        </label>
+        <button className="form__button">Przelicz</button>
       </p>
-    </fieldset>
-    <p>
-      <button className="form__button">Przelicz</button>
-    </p>
-  </form>
-)
+    </form>
+  )
+}
 
 export default Form;
